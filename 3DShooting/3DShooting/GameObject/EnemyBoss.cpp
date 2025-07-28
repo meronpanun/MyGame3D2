44728d898672a8f5b1ab22059f4ca1bf39c508d2
meronpanun <fko2447021@stu.o-hara.ac.jp>
2025-07-28 16:41:45 +0900

    up

--- 3DShooting/3DShooting/GameObject/EnemyBoss.cpp
+++ 3DShooting/3DShooting/GameObject/EnemyBoss.cpp
@@ -1,1 +1,21 @@
 ﻿#include "EnemyBoss.h"
+
+EnemyBoss::EnemyBoss()
+{
+}
+
+EnemyBoss::~EnemyBoss()
+{
+}
+
+void EnemyBoss::Init()
+{
+}
+
+void EnemyBoss::Update(std::vector<Bullet>& bullets, const Player::TackleInfo& tackleInfo, const Player& player, const std::vector<EnemyBase*>& enemyList)
+{
+}
+
+void EnemyBoss::Draw()
+{
+}
--- 3DShooting/3DShooting/GameObject/EnemyBoss.h
+++ 3DShooting/3DShooting/GameObject/EnemyBoss.h
@@ -1,7 +1,19 @@
 ﻿#pragma once
 #include "EnemyBase.h"
 
+/// <summary>
+/// 敵ボスクラス
+/// </summary>
 class EnemyBoss : public EnemyBase
 {
+public:
+	EnemyBoss();
+	virtual ~EnemyBoss();
+	
+	void Init() override;
+	void Update(std::vector<Bullet>& bullets, const Player::TackleInfo& tackleInfo, const Player& player, const std::vector<EnemyBase*>& enemyList) override;
+	void Draw() override;
+
+private:
 };
 
